c27de631a77b9e67f9d0ca3a834c1458f36767a0
Onno Valkering <onnovalkering@users.noreply.github.com>
2020-03-14 17:50:13 +0100

    build: run CI on both Linux and macOS

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
-PROTOC_VERSION="3.11.2"
-PROTOC_RELEASE="protoc-$PROTOC_VERSION-linux-x86_64.zip"
+if [ `uname` = 'Darwin' ]; then
+    OS='osx'
+else
+    OS='linux'
+fi
+
+PROTOC_VERSION="3.11.4"
+PROTOC_RELEASE="protoc-$PROTOC_VERSION-$OS-x86_64.zip"
 
 curl -OL "https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/$PROTOC_RELEASE"
 sudo unzip -o $PROTOC_RELEASE -d /usr/local bin/protoc
